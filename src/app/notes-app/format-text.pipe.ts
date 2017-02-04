e599f21335e5a7094993eceae04cda99b47b56dc
Paul-Antoine <palevasseur@gmail.com>
2017-02-04 16:59:09 +0100

    support plantuml

--- src/app/notes-app/format-text.pipe.ts
+++ src/app/notes-app/format-text.pipe.ts
@@ -1,11 +1,39 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import {plantUrl} from "../utils/plantuml";
 
-@Pipe({ name: 'formatText' })
+@Pipe({name: 'formatText'})
 export class FormatTextPipe implements PipeTransform {
   transform(text: string) {
-    return text.split('\n').reduce( (valPre, valCur) => {
+    // tab = 2 spaces
+    text = text.replace(/\t/g, '  ');
+
+    // html link
+    text = text.replace(/(https?:\/\/[^ \n\r()]+)/gi, (match, $1) => {
+      return '<a target="_top" href="' + $1 + '" >' + $1 + '</a>';
+    });
+
+    // uml tag => uml diagram
+    text = text.replace(/@startuml((?:.|\n|\r)*?)@enduml/gi, (match, $1) => {
+      return '<img src="' + plantUrl($1) + '" />';
+    });
+
+    // space => &nbsp;
+    text = text.replace(/^( *)/gm, (match, $1) => {
+      let nbrSpaces = match.length;
+      let res = '';
+      while (nbrSpaces) {
+        res += '&nbsp;&nbsp;';
+        nbrSpaces--;
+      }
+      return res;
+    });
+
+    // endline => <br>
+    text = text.split('\n').reduce((valPre, valCur) => {
       return valPre + '<br>' + valCur;
     });
+
+    return text;
   }
 
 }
